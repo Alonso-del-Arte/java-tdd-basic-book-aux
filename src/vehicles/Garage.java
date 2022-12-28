b929554e7855f45988f1e5e5fe4f800300ff4512
Alonso del Arte <alonso.delarte@gmail.com>
2022-12-27 19:18:19 -0500

    Starting vehicles package

---
+++ src/vehicles/Garage.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vehicles;
+
+public class Garage {
+    
+    Car storedCar;
+    
+    Garage(Car car) {
+        this.storedCar = car;
+    }
+    
+}
