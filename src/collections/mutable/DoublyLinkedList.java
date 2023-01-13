97d40c586f574737d3fbf32d0df75a3ae51c5be9
Alonso del Arte <alonso.delarte@gmail.com>
2023-01-13 15:24:25 -0500

    Passing toString( ) test

--- src/collections/mutable/DoublyLinkedList.java
+++ src/collections/mutable/DoublyLinkedList.java
@@ -23,14 +23,29 @@ public class DoublyLinkedList<E> {
     
     private Node<E> finish = null;
     
+    private void addVeryFirst(E element) {
+        Node<E> node = new Node<>(element);
+        this.start = node;
+        this.finish = node;
+        this.elemCount = 1;
+        this.hasElements = true;
+    }
+    
     public void addAtBeginning(E element) {
         if (element == null) {
             String excMsg = "Can't add null at beginning nor end of list";
             throw new NullPointerException(excMsg);
         }
         this.mostRecentlyAdded = element;
-        this.elemCount++;
-        this.hasElements = true;
+        if (this.elemCount == 0) {
+            this.addVeryFirst(element);
+        } else {
+            Node<E> node = new Node<>(element);
+            node.next = this.start;
+            this.start.previous = node;
+            this.start = node;
+            this.elemCount++;
+        }
     }
     
     public void addAtEnd(E element) {
@@ -39,8 +54,15 @@ public class DoublyLinkedList<E> {
             throw new NullPointerException(excMsg);
         }
         this.mostRecentlyAdded = element;
-        this.elemCount++;
-        this.hasElements = true;
+        if (this.elemCount == 0) {
+            this.addVeryFirst(element);
+        } else {
+            Node<E> node = new Node<>(element);
+            node.previous = this.finish;
+            this.finish.next = node;
+            this.finish = node;
+            this.elemCount++;
+        }
     }
     
     // TODO: Write tests for this
@@ -81,6 +103,22 @@ public class DoublyLinkedList<E> {
     
     public void clear() {
         // TODO: Write tests for this
+    }
+    
+    @Override
+    public String toString() {
+        String intermediate = "[";
+        boolean keepGoing = this.elemCount > 0;
+        Node<E> curr = this.start;
+        while (keepGoing) {
+            intermediate += curr.elem.toString();
+            keepGoing = curr.next != null;
+            if (keepGoing) {
+                intermediate += ", ";
+                curr = curr.next;
+            }
+        }
+        return intermediate + ']';
     }
     
     public DoublyLinkedList() {
