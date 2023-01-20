076327f2b950082a9cb8a2f0fe95573ff5cf3496
Alonso del Arte <alonso.delarte@gmail.com>
2023-01-20 15:55:53 -0500

    New enumerated type

---
+++ src/time/DayOfWeek.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package time;
+
+/**
+ * Represents a day of the week. This is only for the sake of illustration. For 
+ * actual production work, use <code>java.time.DayOfWeek</code> instead.
+ * @author Alonso del Arte
+ */
+public enum DayOfWeek {
+    
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+    
+}
