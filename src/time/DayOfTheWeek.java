b846c355dc0b0f3b8c8eeb2f40f83ea30b876788
Alonso del Arte <alonso.delarte@gmail.com>
2023-01-23 23:19:52 -0500

    Renaming DayOfWeek enum

---
+++ src/time/DayOfTheWeek.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package time;
+
+/**
+ * Represents a day of the week. This is only for the sake of illustration. For 
+ * actual production work, use <code>java.time.DayOfTheWeek</code> instead.
+ * @author Alonso del Arte
+ */
+public enum DayOfTheWeek {
+    
+    SUNDAY("SUN"), MONDAY("MON"), TUESDAY("TUE"), WEDNESDAY("WED"), 
+    THURSDAY("THU"), FRIDAY("FRI"), SATURDAY("SAT");
+    
+    private final String threeLetterAbbrev;
+    
+    DayOfTheWeek(String abbrev) {
+        this.threeLetterAbbrev = abbrev;
+    }
+}
