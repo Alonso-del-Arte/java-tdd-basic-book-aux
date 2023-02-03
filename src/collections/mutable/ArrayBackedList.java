5fcad4dcbe23affbdbb171bd7000e9c6327120ba
Alonso del Arte <alonso.delarte@gmail.com>
2023-02-03 14:52:01 -0500

    Sort of rewind for hashCode( ), still passes test

--- src/collections/mutable/ArrayBackedList.java
+++ src/collections/mutable/ArrayBackedList.java
@@ -118,11 +118,12 @@ public class ArrayBackedList<E> extends ArrayBackedCollection<E> {
     
     @Override
     public int hashCode() {
-        int hash = 7;
-        for (int i = 0; i < this.count; i++) {
-            hash = 31 * hash + this.elements[i].hashCode();
-        }
-        return (hash << 8) + this.count;
+//        int hash = 7;
+//        for (int i = 0; i < this.count; i++) {
+//            hash = 31 * hash + this.elements[i].hashCode();
+//        }
+//        return (hash << 8) + this.count;
+        return 256 + this.count;
     }
     
     public ArrayBackedList() {
