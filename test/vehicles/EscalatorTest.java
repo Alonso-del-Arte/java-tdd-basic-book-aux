de4316138647732ba325e59dc2154d7537200ae3
Alonso del Arte <alonso.delarte@gmail.com>
2023-04-13 00:32:07 -0400

    Failing interface implementation tests

--- test/vehicles/EscalatorTest.java
+++ test/vehicles/EscalatorTest.java
@@ -11,9 +11,39 @@ import static org.junit.Assert.*;
 public class EscalatorTest {
     
     @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testMark() {
+        System.out.println("mark");
+        Escalator instance = new Escalator();
+        String expected = "Escalator";
+        String actual = instance.mark();
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void testMarkWithASCII() {
+        System.out.println("markWithASCII");
+        Escalator instance = new Escalator();
+        String expected = "Escalator(TM)";
+        String actual = instance.markWithASCII();
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void testMarkWithHTML() {
+        System.out.println("markWithHTML");
+        Escalator instance = new Escalator();
+        String expected = "Escalator&trade;";
+        String actual = instance.markWithHTML();
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void testMarkWithUnicode() {
+        System.out.println("markWithUnicode");
+        Escalator instance = new Escalator();
+        String expected = "Escalator\u2122";
+        String actual = instance.markWithUnicode();
+        assertEquals(expected, actual);
     }
     
 }
