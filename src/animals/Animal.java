ba2c34edd9eca01b29a534d7c2edde333a4da6a6
Alonso del Arte <alonso.delarte@gmail.com>
2023-04-17 14:31:31 -0400

    Workaround for printAnimalInfo( ) problem

--- src/animals/Animal.java
+++ src/animals/Animal.java
@@ -6,8 +6,37 @@ public class Animal {
         return "Animalia";
     }
 
+    public String phylum() {
+        return "Not applicable";
+    }
+
+    public String taxoClass() {
+        return "Not applicable";
+    }
+
+    public String order() {
+        return "Not applicable";
+    }
+
+    public String family() {
+        return "Not applicable";
+    }
+
+    public String species() {
+        return "Not applicable";
+    }
+
     public String getTaxonomicClassification() {
         return "Unknown";
+    }
+
+    public void printAnimalInfo() {
+        System.out.println("Kingdom: " + this.kingdom());
+        System.out.println("Phylum:  " + this.phylum());
+        System.out.println("Class:   " + this.taxoClass());
+        System.out.println("Order:   " + this.order());
+        System.out.println("Family:  " + this.family());
+        System.out.println("Species: " + this.species());
     }
 
     public String call() {
